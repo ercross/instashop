d2ea75956a72e8f68a01ff6d9969199017c83989
ercross <tobins4u@gmail.com>
2024-11-04 17:55:09 +0100

    added endpoint routes

---
+++ api/v1/routes.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func addRoutes() {
+
+}
+
+func authenticationRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Post("/login", login())
+	r.Post("/register", register())
+	return r
+}
+
+func adminRoutes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/products", getAllProducts())
+	r.Get("/product/{id}", getProductByID())
+
+	r.Post("/product", createProduct())
+
+	r.Put("/product/", updateProduct())
+	r.Put("/orders", updateOrderStatus())
+	r.Delete("/products", deleteProduct())
+
+	return r
+}
+
+func orderRoutes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/orders", getOrders())
+	r.Get("/order/{id}", getOrderByID())
+
+	r.Put("/order/cancel", cancelOrder())
+
+	r.Post("/orders", createOrder())
+
+	return r
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module instashop
+
+go 1.23.1
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
